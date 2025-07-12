ff99f83aca836e2e77de1a9c91007268cd51ed67
yildimay <yldry2526@gmail.com>
2025-07-12 20:22:35 +0300

    Update error_fixer_ui.py

--- chemassist/ui/pages/dft/error_fixer_ui.py
+++ chemassist/ui/pages/dft/error_fixer_ui.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 def show_page() -> None:
-    st.header("🧮 DFT • Input Creator")
+    st.header("DFT • Input Creator")
     st.info("This module will let you assemble Gaussian/ORCA/CP2K inputs via a form. \n\n*Implementation coming soon.*")
